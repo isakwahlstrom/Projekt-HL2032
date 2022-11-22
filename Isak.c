bee9c91bcfaff9d760b885b1531a25c8629498c3
Isakwah <114480102+isakwahlstrom@users.noreply.github.com>
2022-11-22 13:43:49 +0100

    Update Isak.c

--- Isak.c
+++ Isak.c
@@ -1,64 +1,49 @@
-/* Including packages and files */ 
 #include "gd32vf103.h"
 #include "lcd.h"
 #include "delay.h"
 #include "gd32v_mpu6500_if.h"
-#include <stdio.h>
-#include "gd32vf103v_eval.h"
-// #include "gd32vf103_it.h"
+#include "math.h"
 
-/* Define global variables */
+// It seems to work but some values are a bit off and we're still getting a lot of disturbance.
 
-#define GRAPH_HEIGHT 30
-#define EI 1
-#define DI 0
+// To fix the disturbance we'll:
+// DONE! create an addapted constant for the initial gravitational pull to compare with the sum value.
+// DONE! add a exakt timer from the internal System Clock.
+// DONE! adjust the calculations of the total acceleration on the IMU
+// DONE! adjust the movementLimit.
+// fine tune from testing.
 
-#define ARRAYNUM(arr_nanme)      (uint32_t)(sizeof(arr_nanme) / sizeof(*(arr_nanme)))
-#define TRANSMIT_SIZE            (ARRAYNUM(txbuffer) - 1)
+// Maybe change the reading frequensy in the MPU driver.
+// Maybe add / change the indicator for a started movement as an change in the total vector of the hand compared to the one on the chest.
 
-/* Create variables for transmit & recive */
-// From Linus
-uint8_t txbuffer[] = "Brassi";
-uint8_t rxbuffer[32];
-uint8_t tx_size = TRANSMIT_SIZE;
-uint8_t rx_size = 32;
-__IO uint8_t txcount = 0; 
-__IO uint16_t rxcount = 0;
-// Our own 
+// Wake-on-motion interrupt for low power operation of applications processor ??? From Datasheet
 
-/* Create a package struct to transmit (transmit and recive data from gyro/acc, vectors) */
-typedef struct {
-    char start = '#';
-    int index;
-    mpu_vector_t Acc;
-    mpu_vector_t Gyro;
-} packet;
+void sqRoot(float nr, float *root);
+int power(int nr, int n);
 
+int main()
+{
+    /* The related data structure for the IMU, contains a vector of x, y, z floats*/
+    // We're just dependent on knowing the Acceleration for this solution. Should we add Gyro, for what purpose?
+    // 2 of them might be needed depending on how we receive  information from the arm.
+    mpu_vector_t Acc, AccHand;
 
-/* Main function */
+    //
+    uint64_t start_mtime, delta_mtime, total;
+    int times = 0;
 
-int main(void) {
-	////////////////////////// Initialize LCD /////////////////////////////////////////
-    
-	Lcd_SetType(LCD_INVERTED);
-    Lcd_Init();
-    LCD_Clear(BLACK);
+    /* Create a State Machine with 3 states: Stand-by, Movement and Calculation */
+    enum StateMachine
+    {
+        Standby,
+        Movement,
+        Calculation
+    };
+    enum StateMachine State, nextState;
+    State = Standby;
+    nextState = Standby;
 
-	/* int to write T/R characters on LCD screen */
-	int x = 0;
-	int y = 0;
-	int x2 = 80;
-	int y2 = 0;
-
-	///////////////////////// Initialize Acc & Gyro ///////////////////////////////////
-
-	/* The related data structure for the IMU, contains a vector of x, y, z floats*/
-    mpu_vector_t vec, vec_temp;
-    mpu_vector_t vec2, vec2_temp;
-    /* for lcd */
-    uint16_t line_color;
-
-	/* Initialize pins for I2C */
+    /* Initialize pins for I2C */
     rcu_periph_clock_enable(RCU_GPIOB);
     rcu_periph_clock_enable(RCU_I2C0);
     gpio_init(GPIOB, GPIO_MODE_AF_OD, GPIO_OSPEED_50MHZ, GPIO_PIN_6 | GPIO_PIN_7);
@@ -67,139 +52,239 @@ int main(void) {
        to change is MPU6500_WHO_AM_I_ID from 0x11 to 0x70. */
     mpu6500_install(I2C0);
 
-	/* Creates temporary vectors to erase previously drwan lines */
-    mpu6500_getAccel(&vec_temp);
-    mpu6500_getGyro(&vec2_temp);
+    /* Initialize LCD */
+    Lcd_SetType(LCD_NORMAL);
+    Lcd_Init();
+    LCD_Clear(1);
 
-	//////////////////////////////////// Initialize Usart //////////////////////////////////////
+    // Create the constant G and a timer (should be changed to a reliable / correct clock)
+    float acctualG = 9.9;
+    float G = 9.806;
+    float time = 0;
+
+    // Acceleration sum and limits to detect motions.
+    float totalAcc = 0;
+    float movmentLimit = 0.05 * G;
+    float velocity = 0;
+    float temp = 0;
+
+    // Navigation indicators, local help variables.
+    float aX, aY, aZ;
+    int negativeX = 0, negativeY = 0, negativeZ = 0;
+    float dAX = 0, dAY = 0, dAZ = 0;
+    float distance = 0;
+    float sumAcc = 0;
+    float integral = 0;
+
+    // Navigation in the room
+    float position[3] = {200.0, 200.0, 200.0};
+    int room[200][200][200];
+    int posX, posY, posZ;
+
+    // Define zones, to enable haptic-feedback
+    char zones[200][200][200];
+
+   
+    for (int x = 100; x <= 130; x++)
+    {
+        for (int y = 70; y <= 130; y++)
+        {
+            for (int z = 60; z <= 100; z++)
+            {
+                zones[x][y][z] = 'G';
+            }
+        }
+    }
+    for (int x = 130; x <= 150; x++)
+    {
+        for (int y = 130; y <= 150; y++)
+        {
+            for (int z = 100; z <= 120; z++)
+            {
+                zones[x][y][z] = 'Y';
+            }
+        }
+    }
+    for (int x = 130; x <= 150; x++)
+    {
+        for (int y = 50; y <= 70; y++)
+        {
+            for (int z = 40; z <= 60; z++)
+            {
+                zones[x][y][z] = 'Y';
+            }
+        }
+    }
+    for (int x = 150; x <= 200; x++)
+    {
+        for (int y = 150; y <= 200; y++)
+        {
+            for (int z = 120; z <= 200; z++)
+            {
+                zones[x][y][z] = 'R';
+            }
+        }
+    }
+    for (int x = 150; x <= 200; x++)
+    {
+        for (int y = 0; y <= 50; y++)
+        {
+            for (int z = 0; z <= 40; z++)
+            {
+                zones[x][y][z] = 'R';
+            }
+        }
+    }
+
+    int giveFeedback = 0;
     
-	//u0init(EI);
+    while (1)
+    {
+        if(totalAcc<17000) {
+            start_mtime = get_timer_value();
+            total=0;
+        }
+        // Get accelleration data (Note: Blocking read) puts a force vector with 1G = 16 384 into x, y, z directions respectively
+        mpu6500_getAccel(&Acc);
 
-	/* USART interrupt configuration */
-    eclic_global_interrupt_enable();
-    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
-    eclic_irq_enable(USART0_IRQn, 1, 0);
-		
-    /* configure COM0 */
-    gd_eval_com_init(EVAL_COM0);                                // Startar hela USART systemet. (u0init(EI) behövs inte)
-	
-    
-	/* enable USART RBNE interrupt */  
-	//usart_interrupt_enable(USART0, USART_INT_RBNE);
+        // Scale the floats of the x, y z direction indicators (Values in G-force)
+        aX = Acc.x;
+        aY = Acc.y;
+        aZ = Acc.z;
 
-	/* Infinity while loop to always check gyro/acc & sedn/recive */
-    
-	//while(1) {
+        // Check if the values are negative, convert negative values
+       /* if (aX < 0)
+        {
+            aX = aX * (-1);
+            negativeX = 1;
+        }
+        if (aY < 0)
+        {
+            aY = aY * (-1);
+            negativeY = 1;
+        }
+        if (aZ < 0)
+        {
+            aZ = aZ * (-1);
+            negativeZ = 1;
+        }
+*/
+        // Alternative to using totalAcc is to always subtract with AccHand, thus it has approximately the same gravitational pull ??
+        // However I think this solution is more reliable or at least more independent.
 
-        ////////////////////////////////// USART ////////////////////////////////////////////////////////////  
+        // Get the total Acc vector, when still it is the gravitational pull from the earth (Seems to be a bit off??)
+        velocity = aX + aZ + aY;
+        totalAcc = (aX * aX) + (aY * aY) + (aZ * aZ);   // 3D - Pythagoras
+        sqRoot(totalAcc, &totalAcc);                    // Should add upp to ca: 1.0 G
+        //totalAcc = totalAcc * G;                      // Should add upp to ca: 9.8 m/s^2
+        // Display the total Acc value
+        LCD_ShowNum1(20, 0,totalAcc, 10, RED);
         
-        /* enable USART TBE interrupt */  
-        usart_interrupt_enable(USART0, USART_INT_TBE);
 
-        // gd32vf103_it har alla dessa variabler med "prefixet extern". betyder nog att det vi skriver eller definerar dom som här kommer finnas sparat där
-        // Därmed behöver vi inte skicka eller ta emot i main utan bara omdefinera txbuffer till det vi vill skicka samt skriva ut rxbuffer när vi tagit emot.
-        // Flagga att vi skickar resp tar emot data. Med While looparna ??
-        // Vi provar det och om det fungerar provar vi ändra om txbuffer till en struct av nått slag ? Även ändras i gd32vf103 då ?
-
-        // Går det verkligen att först skicka och sedan ta emot ??? eller måste vi göra både och samtidigt när vi bara har en RISC-V ? Vart sparas datan där emellan ?   
-
-        LCD_ShowNum(40,40,txcount,6,RED);
-        LCD_Wait_On_Queue();
-        while(txcount < tx_size);       //Skickar det som finns i txbuffer, genom att detta är en "flagga" som startar interrupten. (se kod i gd32vf103_it.c)
-        LCD_ShowNum(20,20,txcount,6,BLUE);
-        LCD_Wait_On_Queue();
-        delay_1ms(250);
-
-        //while(RESET == usart_flag_get(USART0, USART_FLAG_TC));      //Transmission complete?
-        usart_interrupt_enable(USART0, USART_INT_RBNE);
-
-        while(rxcount < rx_size);
-        if(rxcount == rx_size) {
-            LCD_Clear(RED);
-            //LCD_ShowString(10,10,"USART SUCC",BLUE);
+        // Convert the values from G to m/s^2
+        //aX = aX * G;
+        //aY = aY * G;
+        //aZ = aZ * G;
+        LCD_ShowNum1(20, 50, velocity, 10, BLUE);
+        
+        if(totalAcc>17000) {
+            delta_mtime = get_timer_value() - start_mtime;
+            delta_mtime = delta_mtime/(SystemCoreClock/4000);
+            total += delta_mtime;
+            sumAcc += aX * delta_mtime;
+            start_mtime = get_timer_value();
+            //delay_1ms(1000);
         }
         
 
-        ///////////////////////////////////////////////// Accel //////////////////////////////////////////////////////
-		
-        //LCD_ShowChar(20,20,'X',TRANSPARENT, line_color);
-        //LCD_ShowChar(20,40,'Y',TRANSPARENT, line_color);
-        //LCD_ShowChar(20,60,'Z',TRANSPARENT, line_color);
-        //mpu6500_getGyroAccel(&vec2,&vec);
-        // Calls on getAccel and getGyro at the same time
+       if(totalAcc>17000 && total!=0) {
+            total = total/1000;
+            
+            sumAcc = sumAcc/16384;
+            
+            //sumAcc = sumAcc -3;
+            //if(temp!=0) {
+            LCD_ShowNum1(20, 60, total,6,YELLOW);
+            LCD_ShowNum1(20, 30, sumAcc, 6, BLUE);
+            //temp = sumAcc;
+            //}
+           // total = 0;
+            //sumAcc = 0;
+        }
 
-        /* Get accelleration data (Note: Blocking read) puts a force vector with 1G = 4096 into x, y, z directions respectively */
-        mpu6500_getAccel(&vec);
+        /*
+        Sätta in koden i Switch satsen (state machine).
+        Ändra gränsen för rörelse, använda Vel istället för Acc, lägg till en bra död zon.
+        Summera hastigheterna istället för Ax * time.
+        Rensa upp koden.
+        */
 
-        /* Do some fancy math to make a nice display */
+        State = nextState;
 
-        /* Green if pointing up, red if down */
-        line_color = (vec.z < 0) ? RED : GREEN;
-        /* Draw a unit circle (1G) */
-        //Draw_Circle(40, 80 / 2, 28, BLUE);
-        /* Erase last line */
-        //LCD_DrawLine(40, 80 / 2, (40) + (vec_temp.y) / (4096 / 28), (80 / 2) + (vec_temp.x / (4096 / 28)), BLACK);
-        /* Draw new line, scaled to the unit circle */
-        //LCD_DrawLine(40, 80 / 2, (40) + (vec.y) / (4096 / 28), (80 / 2) + (vec.x / (4096 / 28)), line_color);
-        /* Store the last vector in temporary so it can be erased */
-        
-        //LCD_ShowNum1(40,20,vec.x,6,line_color);
-        //LCD_ShowNum1(40,40,vec.y,6,line_color);
-        //LCD_ShowNum1(40,60,vec.z,6,line_color);
+        switch (State)
+        {
+        case Movement:
+         
 
-        vec_temp = vec;
+            // Add a repetition to a position in the room
+            posX = position[0];
+            posY = position[1];
+            posZ = position[2];
+            room[posX][posY][posZ] += 1;
+            break;
+        default:
+            break;
+        }
 
-        //printf("%f", vec.x);
+        // In the chest device we will get the values form the hand:
+        // in a simular fasion as above reciving posX, posY, posZ.
+        // or
+        // just reciving the Acc and making doubble calculations here.
+        // We then compare the two to get an acctual / isolated distanse of the hand.
 
-        // LCD_DrawLine(40, 80 / 2, (40) + (vec_temp.y) / (4096 / 28), (80 / 2) + (vec_temp.x / (4096 / 28)), BLACK);
-        // "Upp" --> upp, "ner" --> Ner, Vänster --> vänster, Höger --> höger
-        // Positivt y = lutar mot sladden           |  Negativt y = lutar bort från sladden          (Skickar y värdet som x värde in till drawLine)
-        // Positivt x = lutar bort från knappsatsen |  Negativt x = lutar mot knappsatsen            (Skickar x värdet som y värde in till drawLine)
-
-        // Längden på linjen beror på 
-
-        // När vi ändrar färg beroende på z. line_color = (vec2.z < 0) ? RED : GREEN; LCD_NORMAL
-        // Skiftar mellan rött o grönt beroende på om den är rättvänd eller upp och ner.
-        // Rött = negativt = upp och ner | Grönt = positivt = rättvänd
-        
-        
-        /* Wait for LCD to finish drawing */
-        LCD_Wait_On_Queue();
-        
-        ///////////////////////////////////////////////// Gyro /////////////////////////////////////////////////////
-
-        /* Get Gyro data (Note: Blocking read???) puts a angular vector with degrees / sec into x, y, z directions respectively */
-        //mpu6500_getGyro(&vec2);
-
-        /* Do some fancy math to make a nice display */
-
-        /* Green if pointing up, red if down */
-        //line_color = (vec2.z < 0) ? RED : GREEN;
-        /* Draw a unit circle (1G)  adapt to Gyro? */
-        //Draw_Circle(120, 80 / 2, 28, BLUE);
-        /* Erase last line */
-        //LCD_DrawLine(120, 80 / 2, (120) + (-1*(vec2_temp.x)) / (2000 / 28), (80 / 2) + (vec2_temp.y / (2000 / 28)), BLACK);
-        /* Draw new line, scaled to the unit circle */
-        //LCD_DrawLine(120, 80 / 2, (120) + (-1*(vec2.x)) / (2000 / 28), (80 / 2) + (vec2.y / (2000 / 28)), line_color);
-        /* Store the last vector so it can be erased */
-        //vec2_temp = vec2;
-
-        // LCD_DrawLine(120, 80 / 2, (120) + (vec2.x) / (2000 / 28), (80 / 2) + (vec2.y / (2000 / 28)), line_color);
-        // "Upp" --> upp, "ner" --> Ner, Vänster --> höger, Höger --> vänster
-        // Positivt x = lutar bort från sladden |  Negativt x = lutar mot sladden               "Rotation runt x axeln"
-        // Positivt y = lutar mot knappsatsen   |  Negativt y = lutar bort från knappsatsen     "Rotation runt z axeln"
-
-        // Längden på linjen beror på hastigheten som vinkeln ändras med.
-        // Hur kan det anpassas eller tydas på ett bra sätt?
-
-        // När vi ändrar färg beroende på z. line_color = (vec2.z < 0) ? RED : GREEN; LCD_NORMAL
-        // Skiftar mellan rött o grönt efter rotationen ? Sladd mot knappsats = rött, tvärtom = grönt.
-        // Grönt = moturs = positivt z | Rött = medurs = negativt z
-        // "Rotation runt y axeln"
-
-        
-        /* Wait for LCD to finish drawing */
-        //LCD_Wait_On_Queue();
-		
-    //};
+        // Show the new position
+        LCD_ShowNum1(100, 10, temp, 5, GREEN);
+        LCD_ShowNum1(100, 30, position[1], 5, GREEN);
+        LCD_ShowNum1(100, 50, position[2], 5, GREEN);
+        //delay_1ms(50);
+    }
 }
+
+void sqRoot(float nr, float *root)
+{
+    int i = 1, j = 1;
+    float x0 = 1.0, xn = 1.0;
+
+    for (i = 1, j = 1; i < nr; i = i * 10, j++)
+        if (nr / i == 0)
+            i = nr;
+    i = i / 10;
+    j = j - 1;
+    if (j > 1)
+        x0 = j * power(10, j / 2);
+
+    for (int a = 1; a <= 10; a++)
+    {
+        xn = 0.5 * (x0 + (nr / x0));
+        x0 = xn;
+    }
+
+    *root = xn;
+}
+
+int power(int nr, int n)
+{
+    int pow = 1;
+    int i;
+    for (i = 1; i < n; i++)
+        pow = pow * nr;
+    return pow;
+}
+
+/*
+    Ta in acc värden, Subtrahera med G också.
+    Beräkna integral av hastighet mellan accelartion peak och pit.
+    Använd deltatiden för att få tiden mellan samples.
+    Rita ut en rektangel som blir större eller mindre beroende på våra rörelser, beroende av denna integral
+    Integraler kan fås med addition, diskreta tidssamples.
+*/
