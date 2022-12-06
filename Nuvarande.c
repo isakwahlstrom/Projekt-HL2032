b0099399e36e38fb3b94e0619d42748810ba6720
KarlHi <113791531+KarlHi@users.noreply.github.com>
2022-12-06 18:36:41 +0100

    Update Nuvarande.c

--- Nuvarande.c
+++ Nuvarande.c
@@ -8,26 +8,14 @@
 #include "gd32v_tf_card_if.h"
 #include "pwm.h"
 
-// Fungerar nu som vi vill :)
-// Just nu saknas bara att:
-// Ändra i drivern för mpu6500 enligt Linus instruktioner på måndag så vi kan läsa av data från den separata IMUn.
-// Och sedan då läsa in den andra sensorn på samma vis och lyckas skilja på dom, mha Linus.
-
 ////////////////////////// Define functions /////////////////////////////////////////
 void Initialize_Project();
 
 void SendToSD(int data);
 
-unsigned int convert_int_to_float (float f);
-
 ///////////////////////////// Main function /////////////////////////////////////////
-int main(void) {
+int main(void) {   
     Initialize_Project();
-        
-    T1setPWMmotorB(1);
-    delay_1ms(1000);
-    T1setPWMmotorB(0);
-    delay_1ms(1000);
     
 	///////////////////////// Acc & Gyro variables ///////////////////////////////////
 
@@ -64,50 +52,56 @@ int main(void) {
             // On a break 
             start_mtime = get_timer_value();
             if(S == 0){
-                LCD_Clear(1);
+                //LCD_Clear(1);
+                SendToSD(redReps);                                  // Send redReps to SD-Card
                 S=1;
                 G=0;
                 R=0;
+                
             }
+            T1setPWMmotorB(1);
+            delay_1ms(200);
+            T1setPWMmotorB(0);
+            delay_1ms(200);
         } else {
+            mpu6500_getAccel(&Acc);                     // Get accelleration data (Note: Blocking read) puts a force vector with 1G = 4096 into x, y, z directions respectively
+            y = Acc.y / 16384;                          // Scale to G values
+            y = y / 4;
             // Add information from Acc2, our IMU on the wrist 
-
-            mpu6500_getAccel(&Acc);                 // Get accelleration data (Note: Blocking read) puts a force vector with 1G = 4096 into x, y, z directions respectively
-            y = Acc.y / 16384;                      // Scale to G values
-
-            //mpu6500_getAccel(&Acc2);                // Get accelleration data (Note: Blocking read) puts a force vector with 1G = 4096 into x, y, z directions respectively
-            //y2 = (-1)*Acc2.y / 16384;              // Scale to G values
-
+            //mpu6500_getAccel(&Acc2);                  // Get accelleration data (Note: Blocking read) puts a force vector with 1G = 4096 into x, y, z directions respectively
+            //y2 = (-1)*Acc2.y / 16384;                 // Scale to G values
+            // if((y>=0.8) || (y2>=0.9))  {
+            
             // Compare y to the gravitational pull, y is higher then 0.8 G we're in the Green zone
-            if(y>=0.8)  {            // if((y>=0.8) || (y2>=0.9))  {
+            if(y>=0.8)  {            
                 if(G==0){
-                    LCD_Clear(GREEN);
+                    //LCD_Clear(GREEN);
+                    T1setPWMmotorB(0);
                     G=1;
                     R=0;
                 }
-            } else if(y>=0.7){
+            } else if(y>=0.7) {
                 // Dead-zone
-                R=0;
-                G=0;
+                
             } else {
                 if(R==0){
-                    LCD_Clear(RED);
+                    //LCD_Clear(RED);
+                    T1setPWMmotorB(1);
                     redReps += 1;
                     R=1;
                     G=0;
-                    if((redReps%10)==0 && (redReps>0)) {
-                        // Send vibration ... Or.... Light LED
-                        startH_mtime = get_timer_value();
-                    }
+                    // if((redReps%10)==0 && (redReps>0)) {
+                    //     // Send vibration ... Or.... Light LED
+                    //     startH_mtime = get_timer_value();
+                    // }
                 }
-                deltaH_mtime = get_timer_value() - startH_mtime;
-                if (deltaH_mtime < (SystemCoreClock/4000.0 *hepticPeriod)){
-                    T1setPWMmotorB(1);
-                } else T1setPWMmotorB(0);
+                // deltaH_mtime = get_timer_value() - startH_mtime;
+                // if (deltaH_mtime < (SystemCoreClock/4000.0 *hepticPeriod)){
+                //     T1setPWMmotorB(1);
+                // } else T1setPWMmotorB(0);
             }
             S = 0;
         }
-
         /////////////////////////////////////////////////// SD-Card //////////////////////////////////////////////////////////// 
         // Count up the time passed in ms
         delta_mtime = get_timer_value() - start_mtime;
@@ -122,16 +116,15 @@ int main(void) {
 ////////////////////////// Write functions /////////////////////////////////////////
 
 void Initialize_Project(){
+    ////////////////////////////////// Initialize  heptic feedback //////////////////////////////////
+    T1powerUpInitPWM(0x3);                  //Starts A0 and A1.
     ////////////////////////////////////// Initialize LCD ///////////////////////////////////////////
-	 
-    Lcd_SetType(LCD_INVERTED);
-    Lcd_Init();
-    LCD_Clear(1);
-    LCD_DrawPoint(1,1,1);
-    delay_1ms(100);
-    
-	//////////////////////////////////// Initialize Acc & Gyro /////////////////////////////////////
-
+    // Lcd_SetType(LCD_INVERTED);
+    // Lcd_Init();
+    // LCD_Clear(1);
+    // LCD_DrawPoint(1,1,1);
+    // delay_1ms(100);
+	//////////////////////////////////// Initialize Acc  ///////////////////////////////////////////
 	/* Initialize pins for I2C */
     rcu_periph_clock_enable(RCU_GPIOB);
     rcu_periph_clock_enable(RCU_I2C0);
@@ -141,13 +134,16 @@ void Initialize_Project(){
        to change is MPU6500_WHO_AM_I_ID from 0x11 to 0x70. */
 
     mpu6500_install(I2C0);
-
+    //////////////////////////////////// Initialize Button  ///////////////////////////////////////////
     rcu_periph_clock_enable(RCU_GPIOA);
     /* This configures the A3 and A4 pins as inputs with internal pull ups enabled */
     gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
-
-    /* Init pins for heptic feedback. */
-    T1powerUpInitPWM(0x3);                  //Starts A0 and A1.
+    
+    /* Blink diod */
+    T1setPWMmotorB(1);
+    delay_1ms(1000);
+    T1setPWMmotorB(0);
+    delay_1ms(1000);
 }
 
 void SendToSD(int data){
@@ -159,7 +155,7 @@ void SendToSD(int data){
 
     char information[128];
     
-    set_fattime(2022,10,12,0,0,0);
+    set_fattime(2022,12,6,12,0,0);
     delay_1ms(100);
     
     strcpy(information,"");
@@ -169,7 +165,7 @@ void SendToSD(int data){
     f_mount(&fs,"",1);
     f_sync(&file);
 
-    fr = f_open(&file, "DATA.TXT", FA_WRITE | FA_OPEN_APPEND);
+    fr = f_open(&file, "DATA.TXT", FA_WRITE | FA_CREATE_ALWAYS);
     fr = f_write(&file, information, strlen(information), &bw);
     delay_1ms(400);
 
@@ -178,14 +174,4 @@ void SendToSD(int data){
     f_close(&file);
     delay_1ms(100);
 
-}
-
-unsigned int convert_int_to_float ( float f) {
-    int integer, decimal;
-    integer = f;
-    decimal = (f - integer ) * 10;
-    if (decimal >= 5){
-        integer++;
-    }
-    return integer;
 }
